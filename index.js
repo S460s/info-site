8c7c552ff29060aac27330065b8dc30708a003e0
S460 <aleksandar.ta6ev@gmail.com>
2021-06-06 11:11:34 +0300

    Added very simple style

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
+express.static(__dirname);
+
 const app = express();
 const PORT = 8080;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 	res.sendFile('routes/index.html', { root: __dirname });
